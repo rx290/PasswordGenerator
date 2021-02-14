183a8812d49e8680209f5bcc03611a8f57953678
Muhammad As'ad Waseem <asadwaseem32@gmail.com>
2021-02-15 00:55:11 +0500

    Added Comments

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
-
+# Importing Tkinter for GUI Interface
 import tkinter as tk
+
+#importing String for ASCII characters
 import string
+
+#Importing Random library to generate and choose random characters to create passwords
 import random
+
+# Os library to check paths i.e. if the file exist or not
 import os.path
 from os import path
+
+# Importing datetime module to cater todays date
 from datetime import date
 
+# initializing date Module for current date
 today = date.today()
+
+# Removing extra variables like white space, tabs and other form of format specifiers
 characters = string.printable[0:-6]
-# Sep-16-2019
+
+# formating date like thi Sep-16-2019
 formated_date = today.strftime("%b-%d-%Y")
 
-
+# Initializing tkinter app
 app = tk.Tk()
+
+# seeting up geometry for app i.e. windows size
 app.geometry("400x300")
 
 
-
+# Long string for menu printing
 selection = int(input("""
 
 #######################
@@ -29,16 +43,18 @@ selection = int(input("""
 3. close
 Please Enter Your Choice:"""))
 
-# generated_pass= ""
+generated_pass= ""
 
 def passgencli():
 
     password_length = int(input("Please Enter Password Length:"))
     password = ""
 
+    # looping over the length and choosing random characters to generate a password
     for i in range(password_length+1):
         password +=random.choice(characters)
     
+    # Checking if the file exist or not, if it doesn't then create a new one otherwise append the existing one
     if (path.exists("Generated_Passwords.txt")):
         f = open("Generated_Passwords.txt", "a")
         f.write(formated_date + "\t \t \t \t \t" + password +"\n")
@@ -55,14 +71,14 @@ def passgencli():
 
 
 
-# def passgengui():
+def passgengui():
 
-#     password_length = int(input("Please Enter Password Length:"))
-#     password = ""
+    password_length = int(input("Please Enter Password Length:"))
+    password = ""
 
-#     for i in range(password_length+1):
-#         password +=random.choice(characters)
-#     generated_pass = password
+    for i in range(password_length+1):
+        password +=random.choice(characters)
+    generated_pass = password
 
 
 
